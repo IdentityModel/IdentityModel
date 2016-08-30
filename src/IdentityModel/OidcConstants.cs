dbc7812229b4561b1c417901baf2f34a7cbaf03b
Dominick Baier <dbaier@leastprivilege.com>
2016-08-30 09:32:44 +0200

    Added constants for discovery document

--- src/IdentityModel/OidcConstants.cs
+++ src/IdentityModel/OidcConstants.cs
@@ -254,5 +254,58 @@ namespace IdentityModel
 
             }
         }
+
+        public static class Discovery
+        {
+            public const string Issuer                                      = "issuer";
+
+            // endpoints
+            public const string AuthorizationEndpoint                       = "authorization_endpoint";
+            public const string TokenEndpoint                               = "token_endpoint";
+            public const string UserInfoEndpoint                            = "userinfo_endpoint";
+            public const string IntrospectionEndpoint                       = "introspection_endpoint";
+            public const string RevocationEndpoint                          = "revocation_endpoint";
+            public const string DiscoveryEndpoint                           = ".well-known/openid-configuration";
+            public const string JwksUri                                     = "jwks_uri";
+            public const string EndSessionEndpoint                          = "end_session_endpoint";
+            public const string CheckSessionIframe                          = "check_session_iframe";
+            public const string RegistrationEndpoint                        = "registration_endpoint";
+
+            // common capabilities
+            public const string FrontChannelLogoutSupported                 = "frontchannel_logout_supported";
+            public const string FrontChannelLogoutSessionSupported          = "frontchannel_logout_session_supported";
+            public const string GrantTypesSupported                         = "grant_types_supported";
+            public const string CodeChallengeMethodsSupported               = "code_challenge_methods_supported";
+            public const string ScopesSupported                             = "scopes_supported";
+            public const string SubjectTypesSupported                       = "subject_types_supported";
+            public const string ResponseModesSupported                      = "response_modes_supported";
+            public const string ResponseTypesSupported                      = "response_types_supported";
+            public const string ClaimsSupported                             = "claims_supported";
+            public const string TokenEndpointAuthenticationMethodsSupported = "token_endpoint_auth_methods_supported";
+
+            // more capabilities
+            public const string ClaimsLocalesSupported                      = "claims_locales_supported";
+            public const string ClaimsParameterSupported                    = "claims_parameter_supported";
+            public const string ClaimTypesSupported                         = "claim_types_supported";
+            public const string DisplayValuesSupported                      = "display_values_supported";
+            public const string AcrValuesSupported                          = "acr_values_supported";
+            public const string IdTokenEncryptionAlgorithmsSupported        = "id_token_encryption_alg_values_supported";
+            public const string IdTokenEncryptionEncValuesSupported         = "id_token_encryption_enc_values_supported";
+            public const string IdTokenSigningAlgorithmsSupported           = "id_token_signing_alg_values_supported";
+            public const string OpPolicyUri                                 = "op_policy_uri";
+            public const string OpTosUri                                    = "op_tos_uri";
+            public const string RequestObjectEncryptionAlgorithmsSupported  = "request_object_encryption_alg_values_supported";
+            public const string RequestObjectEncryptionEncValuesSupported   = "request_object_encryption_enc_values_supported";
+            public const string RequestObjectSigningAlgorithmsSupported     = "request_object_signing_alg_values_supported";
+            public const string RequestParameterSupported                   = "request_parameter_supported";
+            public const string RequestUriParameterSupported                = "request_uri_parameter_supported";
+            public const string RequireRequestUriRegistration               = "require_request_uri_registration";
+            public const string ServiceDocumentation                        = "service_documentation";
+            public const string TokenEndpointAuthSigningAlgorithmsSupported = "token_endpoint_auth_signing_alg_values_supported";
+            public const string UILocalesSupported                          = "ui_locales_supported";
+            public const string UserInfoEncryptionAlgorithmsSupported       = "userinfo_encryption_alg_values_supported";
+            public const string UserInfoEncryptionEncValuesSupported        = "userinfo_encryption_enc_values_supported";
+            public const string UserInfoSigningAlgorithmsSupported          = "userinfo_signing_alg_values_supported";
+        }
     }
 }
