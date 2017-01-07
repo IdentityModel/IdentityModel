a605c9b5b19946781ba90e4eb0a5a0f5b7617c9d
Dominick Baier <dbaier@leastprivilege.com>
2017-01-07 10:19:00 +0100

    add code comments to discovery policy

--- src/IdentityModel/Client/DiscoveryPolicy.cs
+++ src/IdentityModel/Client/DiscoveryPolicy.cs
@@ -9,12 +9,34 @@ namespace IdentityModel.Client
     {
         internal string Authority;
 
-        public bool ValidateIssuerName { get; set; } = true;
-        public bool ValidateEndpoints { get; set; } = true;
-        public bool RequireKeySet { get; set; } = true;
-
+        /// <summary>
+        /// Specifies if HTTPS is enforced on all endpoints. Defaults to true.
+        /// </summary>
         public bool RequireHttps { get; set; } = true;
+
+        /// <summary>
+        /// Specifies if HTTP is allowed on loopback addresses. Defaults to true.
+        /// </summary>
         public bool AllowHttpOnLoopback { get; set; } = true;
+
+        /// <summary>
+        /// Specifies valid loopback addresses, defaults to localhost and 127.0.0.1
+        /// </summary>
         public ICollection<string> LoopbackAddresses = new HashSet<string> { "localhost", "127.0.0.1" };
+
+        /// <summary>
+        /// Specifies if the issuer name is checked to be identical to the authority. Defaults to true.
+        /// </summary>
+        public bool ValidateIssuerName { get; set; } = true;
+
+        /// <summary>
+        /// Specifies if all endpoints are checked to belong to the authority. Defaults to true.
+        /// </summary>
+        public bool ValidateEndpoints { get; set; } = true;
+
+        /// <summary>
+        /// Specifies if a key set is required. Defaults to true.
+        /// </summary>
+        public bool RequireKeySet { get; set; } = true;
     }
 }
