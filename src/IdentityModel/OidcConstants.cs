2055371efad3f18b20673191483ae799772f09c7
Dominick Baier <dbaier@leastprivilege.com>
2017-01-24 09:10:37 +0100

    Add dynamic registration constants

--- src/IdentityModel/OidcConstants.cs
+++ src/IdentityModel/OidcConstants.cs
@@ -120,6 +120,49 @@ namespace IdentityModel
             public const string BearerTokenType     = "Bearer";
         }
 
+        public static class RegistrationResponse
+        {
+            public const string ClientId                = "client_id";
+            public const string ClientSecret            = "client_secret";
+            public const string RegistrationAccessToken = "registration_access_token";
+            public const string RegistrationClientUri   = "registration_client_uri";
+            public const string ClientIdIssuedAt        = "client_id_issued_at";
+            public const string ClientSecretExpiresAt   = "client_secret_expires_at";
+        }
+
+        public static class ClientMetadata
+        {
+            public const string RedirectUris                                = "redirect_uris";
+            public const string ResponseTypes                               = "response_types";
+            public const string GrantTypes                                  = "grant_types";
+            public const string ApplicationType                             = "application_type";
+            public const string Contacts                                    = "contacts";
+            public const string ClientName                                  = "client_name";
+            public const string LogoUri                                     = "logo_uri";
+            public const string ClientUri                                   = "client_uri";
+            public const string PolicyUri                                   = "policy_uri";
+            public const string TosUri                                      = "tos_uri";
+            public const string JwksUri                                     = "jwks_uri";
+            public const string SectorIdentifierUri                         = "sector_identifier_uri";
+            public const string SubjectType                                 = "subject_type";
+            public const string TokenEndpointAuthenticationMethod           = "token_endpoint_auth_method";
+            public const string tokenEndpointAuthenticationSigningAlgorithm = "token_endpoint_auth_signing_alg";
+            public const string DefaultMaxAge                               = "default_max_age";
+            public const string RequireAuthenticationTime                   = "require_auth_time";
+            public const string DefaultAcrValues                            = "default_acr_values";
+            public const string InitiateLoginUris                           = "initiate_login_uri";
+            public const string RequestUris                                 = "request_uris";
+            public const string IdentityTokenSignedResponseAlgorithm        = "id_token_signed_response_alg";
+            public const string IdentityTokenEncryptedResponseAlgorithm     = "id_token_encrypted_response_alg";
+            public const string IdentityTokenEncryptedResponseEncryption    = "id_token_encrypted_response_enc";
+            public const string UserinfoSignedResponseAlgorithm             = "userinfo_signed_response_alg";
+            public const string UserInfoEncryptedResponseAlgorithm          = "userinfo_encrypted_response_alg";
+            public const string UserinfoEncryptedResponseEncryption         = "userinfo_encrypted_response_enc";
+            public const string RequestObjectSigningAlgorithm               = "request_object_signing_alg";
+            public const string RequestObjectEncryptionAlgorithm            = "request_object_encryption_alg";
+            public const string RequestObjectEncryptionEncryption           = "request_object_encryption_enc";
+        }
+        
         public static class TokenTypes
         {
             public const string AccessToken   = "access_token";
