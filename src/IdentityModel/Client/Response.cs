962e5a5c26181c2551cd9dad3362309b3b990207
Dominick Baier <dbaier@leastprivilege.com>
2017-01-24 09:34:49 +0100

    add base class for protocol response

---
+++ src/IdentityModel/Client/Response.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace IdentityModel.Client
+{
+    public abstract class Response
+    {
+        public Response(string raw)
+        {
+            Raw = raw;
+
+            try
+            {
+                Json = JObject.Parse(raw);
+            }
+            catch (Exception ex)
+            {
+                ErrorType = ResponseErrorType.Exception;
+                Exception = ex;
+
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Error))
+            {
+                HttpStatusCode = HttpStatusCode.OK;
+            }
+            else
+            {
+                HttpStatusCode = HttpStatusCode.BadRequest;
+                ErrorType = ResponseErrorType.Protocol;
+            }
+        }
+
+        public Response(Exception exception)
+        {
+            Exception = exception;
+            ErrorType = ResponseErrorType.Exception;
+        }
+
+        public Response(HttpStatusCode statusCode, string reason)
+        {
+            ErrorType = ResponseErrorType.Http;
+            HttpStatusCode = statusCode;
+            HttpErrorReason = reason;
+        }
+
+        public string Raw { get; }
+        public JObject Json { get; }
+        public Exception Exception { get; set; }
+
+        public bool IsError => !string.IsNullOrEmpty(Error);
+        public ResponseErrorType ErrorType { get; } = ResponseErrorType.None;
+        public HttpStatusCode HttpStatusCode { get; }
+        public string HttpErrorReason { get; }
+
+        public string Error
+        {
+            get
+            {
+                if (ErrorType == ResponseErrorType.Http)
+                {
+                    return HttpErrorReason;
+                }
+                else if (ErrorType == ResponseErrorType.Exception)
+                {
+                    return Exception.Message;
+                }
+
+                return TryGet(OidcConstants.TokenResponse.Error);
+            }
+        }
+
+        public string TryGet(string name) => Json.TryGetString(name);
+    }
+}
--- src/IdentityModel/Client/TokenResponse.cs
+++ src/IdentityModel/Client/TokenResponse.cs
@@ -1,74 +1,29 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using Newtonsoft.Json.Linq;
 using System;
 using System.Net;
 
 namespace IdentityModel.Client
 {
-    public class TokenResponse
+    public class TokenResponse : Response
     {
-        public TokenResponse(string raw)
+        public TokenResponse(string raw) : base(raw)
         {
-            Raw = raw;
-
-            try
-            {
-                Json = JObject.Parse(raw);
-            }
-            catch (Exception ex)
-            {
-                IsError = true;
-                ErrorType = ResponseErrorType.Exception;
-                Exception = ex;
-
-                return;
-            }
-
-            if (string.IsNullOrWhiteSpace(Error))
-            {
-                IsError = false;
-                HttpStatusCode = HttpStatusCode.OK;
-            }
-            else
-            {
-                IsError = true;
-                HttpStatusCode = HttpStatusCode.BadRequest;
-                ErrorType = ResponseErrorType.Protocol;
-            }
-        }
-        
-        public TokenResponse(Exception exception)
-        {
-            IsError = true;
-
-            Exception = exception;
-            ErrorType = ResponseErrorType.Exception;
         }
 
-        public TokenResponse(HttpStatusCode statusCode, string reason)
+        public TokenResponse(Exception exception) : base(exception)
         {
-            IsError = true;
-
-            ErrorType = ResponseErrorType.Http;
-            HttpStatusCode = statusCode;
-            HttpErrorReason = reason;
         }
 
-        public string Raw { get; }
-        public JObject Json { get; }
-        public Exception Exception { get; set; }
+        public TokenResponse(HttpStatusCode statusCode, string reason) : base(statusCode, reason)
+        {
+        }
 
-        public bool IsError { get; }
-        public ResponseErrorType ErrorType { get; } = ResponseErrorType.None;
-        public HttpStatusCode HttpStatusCode { get; }
-        public string HttpErrorReason { get; }
-
-        public string AccessToken => TryGet(OidcConstants.TokenResponse.AccessToken);
-        public string IdentityToken => TryGet(OidcConstants.TokenResponse.IdentityToken);
-        public string TokenType => TryGet(OidcConstants.TokenResponse.TokenType);
-        public string RefreshToken => TryGet(OidcConstants.TokenResponse.RefreshToken);
+        public string AccessToken      => TryGet(OidcConstants.TokenResponse.AccessToken);
+        public string IdentityToken    => TryGet(OidcConstants.TokenResponse.IdentityToken);
+        public string TokenType        => TryGet(OidcConstants.TokenResponse.TokenType);
+        public string RefreshToken     => TryGet(OidcConstants.TokenResponse.RefreshToken);
         public string ErrorDescription => TryGet(OidcConstants.TokenResponse.ErrorDescription);
 
         public long ExpiresIn
@@ -89,24 +44,5 @@ namespace IdentityModel.Client
                 return 0;
             }
         }
-
-        public string Error
-        {
-            get
-            {
-                if (ErrorType == ResponseErrorType.Http)
-                {
-                    return HttpErrorReason;
-                }
-                else if (ErrorType == ResponseErrorType.Exception)
-                {
-                    return Exception.Message;
-                }
-
-                return TryGet(OidcConstants.TokenResponse.Error);
-            }
-        }
-
-        public string TryGet(string name) => Json.TryGetString(name);
     }
 }
