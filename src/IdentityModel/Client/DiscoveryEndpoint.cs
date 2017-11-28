e51ba652bfebcbb6a4e4e40f61ed1c34f29fa6d6
Dominick Baier <dbaier@leastprivilege.com>
2017-11-28 07:04:50 +0100

    cleanup

--- src/IdentityModel/Client/DiscoveryEndpoint.cs
+++ src/IdentityModel/Client/DiscoveryEndpoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdentityModel.Client
+﻿namespace IdentityModel.Client
 {
     /// <summary>
     /// Represents a URL to a discovery endpoint - parsed to separate the URL and authority
