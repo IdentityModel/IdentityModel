952477fe3d929dc5a1d2a1cfb3b8991fb879814f
Dominick Baier <dbaier@leastprivilege.com>
2017-11-28 08:07:12 +0100

    added tests for RequestUrl

--- test/UnitTests/RequestUrlTests.cs
+++ test/UnitTests/RequestUrlTests.cs
@@ -1,11 +1,36 @@
-﻿using FluentAssertions;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using FluentAssertions;
 using IdentityModel.Client;
+using System.Collections.Generic;
 using Xunit;
 
 namespace IdentityModel.UnitTests
 {
-    public class AuthorizeRequestTests
+    public class RequestUrlTests
     {
+        [Fact]
+        public void null_value_should_return_base()
+        {
+            var request = new RequestUrl("http://server/authorize");
+
+            var url = request.Create(null);
+
+            url.Should().Be("http://server/authorize");
+        }
+
+        [Fact]
+        public void empty_value_should_return_base()
+        {
+            var request = new RequestUrl("http://server/authorize");
+
+            var values = new Dictionary<string, string>();
+            var url = request.Create(null);
+
+            url.Should().Be("http://server/authorize");
+        }
+
         [Fact]
         public void Create_absolute_url_should_behave_as_expected()
         {
