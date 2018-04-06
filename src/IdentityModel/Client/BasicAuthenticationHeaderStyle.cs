91854a961f0db671c5384cd17f3e50e705a10631
Dominick Baier <dbaier@leastprivilege.com>
2018-04-06 14:36:52 +0200

    moved enum (breaking change - sorry)

---
+++ src/IdentityModel/Client/BasicAuthenticationHeaderStyle.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace IdentityModel.Client
+{
+    /// <summary>
+    /// Enum for specifying then encoding style of the basic authentication header
+    /// </summary>
+    public enum BasicAuthenticationHeaderStyle
+    {
+        /// <summary>
+        /// Recommended. Uses the encoding as described in the OAuth 2.0 spec (https://tools.ietf.org/html/rfc6749#section-2.3.1). Base64(urlformencode(client_id) + ":" + urlformencode(client_secret))
+        /// </summary>
+        Rfc6749,
+        /// <summary>
+        /// Uses the encoding as described in the original basic authentication spec (https://tools.ietf.org/html/rfc2617#section-2 - used by some non-OAuth 2.0 compliant authorization servers). Base64(client_id + ":" + client_secret). 
+        /// </summary>
+        Rfc2617
+    }
+}
--- src/IdentityModel/Client/TokenClient.cs
+++ src/IdentityModel/Client/TokenClient.cs
@@ -120,7 +120,7 @@ namespace IdentityModel.Client
         /// <value>
         /// The basic authentication header style.
         /// </value>
-        public AuthenticationHeaderStyle BasicAuthenticationHeaderStyle { get; set; } = AuthenticationHeaderStyle.Rfc6749;
+        public BasicAuthenticationHeaderStyle BasicAuthenticationHeaderStyle { get; set; } = BasicAuthenticationHeaderStyle.Rfc6749;
 
         /// <summary>
         /// Gets or sets the address.
@@ -169,11 +169,11 @@ namespace IdentityModel.Client
 
             if (AuthenticationStyle == AuthenticationStyle.BasicAuthentication)
             {
-                if (BasicAuthenticationHeaderStyle == AuthenticationHeaderStyle.Rfc6749)
+                if (BasicAuthenticationHeaderStyle == BasicAuthenticationHeaderStyle.Rfc6749)
                 {
                     request.SetBasicAuthenticationOAuth(ClientId, ClientSecret);
                 }
-                else if (BasicAuthenticationHeaderStyle == AuthenticationHeaderStyle.Rfc2617)
+                else if (BasicAuthenticationHeaderStyle == BasicAuthenticationHeaderStyle.Rfc2617)
                 {
                     request.SetBasicAuthentication(ClientId, ClientSecret);
                 }
@@ -260,21 +260,6 @@ namespace IdentityModel.Client
                 _disposed = true;
                 Client.Dispose();
             }
-        }
-
-        /// <summary>
-        /// Enum for specifying then encoding style of the basic authentication header
-        /// </summary>
-        public enum AuthenticationHeaderStyle
-        {
-            /// <summary>
-            /// Recommended. Uses the encoding as described in the OAuth 2.0 spec (https://tools.ietf.org/html/rfc6749#section-2.3.1). Base64(urlformencode(client_id) + ":" + urlformencode(client_secret))
-            /// </summary>
-            Rfc6749,
-            /// <summary>
-            /// Uses the encoding as described in the original basic authentication spec (https://tools.ietf.org/html/rfc2617#section-2 - used by some non-OAuth 2.0 compliant authorization servers). Base64(client_id + ":" + client_secret). 
-            /// </summary>
-            Rfc2617
         }
     }
 }
