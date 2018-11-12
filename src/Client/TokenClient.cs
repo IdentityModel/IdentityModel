ae2f016f4ebd70c1e9908f0fa62f2e59d18c4154
Dominick Baier <dbaier@leastprivilege.com>
2018-11-12 08:06:46 +0100

    add comments

--- src/Client/TokenClient.cs
+++ src/Client/TokenClient.cs
@@ -6,33 +6,57 @@ using System.Threading.Tasks;
 
 namespace IdentityModel.Client
 {
+    /// <summary>
+    /// Client library for the OpenID Connect / OAuth 2 token endpoint
+    /// </summary>
     public class TokenClient
     {
+        private readonly HttpClient _client;
+        private readonly TokenClientOptions _options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TokenClient"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <param name="options">The options.</param>
+        /// <exception cref="ArgumentNullException">client</exception>
         public TokenClient(HttpClient client, TokenClientOptions options)
         {
-            Client = client ?? throw new ArgumentNullException(nameof(client));
-            Options = options ?? new TokenClientOptions();
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+            _options = options ?? new TokenClientOptions();
         }
 
-        public HttpClient Client { get; }
-
-        private readonly TokenClientOptions Options;
-
+        /// <summary>
+        /// Sets request parameters from the options.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="parameters">The parameters.</param>
         public void ApplyRequestParameters(TokenRequest request, IDictionary<string, string> parameters)
         {
-            request.Address = Options.Address;
-            request.ClientId = Options.ClientId;
-            request.ClientSecret = Options.ClientSecret;
-            request.ClientAssertion = Options.ClientAssertion;
-            request.ClientCredentialStyle = Options.ClientCredentialStyle;
-            request.AuthorizationHeaderStyle = Options.AuthorizationHeaderStyle;
+            request.Address = _options.Address;
+            request.ClientId = _options.ClientId;
+            request.ClientSecret = _options.ClientSecret;
+            request.ClientAssertion = _options.ClientAssertion;
+            request.ClientCredentialStyle = _options.ClientCredentialStyle;
+            request.AuthorizationHeaderStyle = _options.AuthorizationHeaderStyle;
+            request.Parameters = _options.Parameters;
 
             if (parameters != null)
             {
-                request.Parameters = parameters;
+                foreach (var parameter in parameters)
+                {
+                    request.Parameters.Add(parameter);
+                }
             }
         }
 
+        /// <summary>
+        /// Sends a token request using the client_credentials grant type.
+        /// </summary>
+        /// <param name="scope">The scope (space separated string).</param>
+        /// <param name="parameters">Extra parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         public Task<TokenResponse> RequestClientCredentialsTokenAsync(string scope = null, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
         {
             var request = new ClientCredentialsTokenRequest
@@ -41,9 +65,16 @@ namespace IdentityModel.Client
             };
             ApplyRequestParameters(request, parameters);
 
-            return Client.RequestClientCredentialsTokenAsync(request);
+            return _client.RequestClientCredentialsTokenAsync(request);
         }
 
+        /// <summary>
+        /// Sends a token request using the urn:ietf:params:oauth:grant-type:device_code grant type.
+        /// </summary>
+        /// <param name="deviceCode">The device code.</param>
+        /// <param name="parameters">Extra parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         public Task<TokenResponse> RequestDeviceTokenAsync(string deviceCode, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
         {
             var request = new DeviceTokenRequest
@@ -52,22 +83,40 @@ namespace IdentityModel.Client
             };
             ApplyRequestParameters(request, parameters);
 
-            return Client.RequestDeviceTokenAsync(request);
+            return _client.RequestDeviceTokenAsync(request);
         }
 
+        /// <summary>
+        /// Sends a token request using the password grant type.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="scope">The scope (space separated string).</param>
+        /// <param name="parameters">Extra parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         public Task<TokenResponse> RequestPasswordTokenAsync(string userName, string password = null, string scope = null, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
         {
             var request = new PasswordTokenRequest
             {
-                UserName = userName, 
+                UserName = userName,
                 Password = password,
                 Scope = scope
             };
             ApplyRequestParameters(request, parameters);
 
-            return Client.RequestPasswordTokenAsync(request);
+            return _client.RequestPasswordTokenAsync(request);
         }
 
+        /// <summary>
+        /// Sends a token request using the authorization_code grant type.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <param name="redirectUri">The redirect URI.</param>
+        /// <param name="codeVerifier">The code verifier.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         public Task<TokenResponse> RequestAuthorizationCodeTokenAsync(string code, string redirectUri, string codeVerifier = null, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
         {
             var request = new AuthorizationCodeTokenRequest
@@ -78,20 +127,17 @@ namespace IdentityModel.Client
             };
             ApplyRequestParameters(request, parameters);
 
-            return Client.RequestAuthorizationCodeTokenAsync(request, cancellationToken);
+            return _client.RequestAuthorizationCodeTokenAsync(request, cancellationToken);
         }
 
-        public Task<TokenResponse> RequestTokenAsync(string grantType, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
-        {
-            var request = new TokenRequest
-            {
-                GrantType = grantType
-            };
-            ApplyRequestParameters(request, parameters);
-
-            return Client.RequestTokenAsync(request, cancellationToken);
-        }
-
+        /// <summary>
+        /// Sends a token request using the refresh_token grant type.
+        /// </summary>
+        /// <param name="refreshToken">The refresh token.</param>
+        /// <param name="scope">The scope (space separated string).</param>
+        /// <param name="parameters">Extra parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         public Task<TokenResponse> RequestRefreshTokenAsync(string refreshToken, string scope = null, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
         {
             var request = new RefreshTokenRequest
@@ -101,7 +147,25 @@ namespace IdentityModel.Client
             };
             ApplyRequestParameters(request, parameters);
 
-            return Client.RequestRefreshTokenAsync(request, cancellationToken);
+            return _client.RequestRefreshTokenAsync(request, cancellationToken);
+        }
+
+        /// <summary>
+        /// Sends a token request.
+        /// </summary>
+        /// <param name="grantType">Type of the grant.</param>
+        /// <param name="parameters">Extra parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public Task<TokenResponse> RequestTokenAsync(string grantType, IDictionary<string, string> parameters = null, CancellationToken cancellationToken = default)
+        {
+            var request = new TokenRequest
+            {
+                GrantType = grantType
+            };
+            ApplyRequestParameters(request, parameters);
+
+            return _client.RequestTokenAsync(request, cancellationToken);
         }
     }
 }
