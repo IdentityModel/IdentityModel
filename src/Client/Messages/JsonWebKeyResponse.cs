92546590f4957a6af968147cf41483dbc756954b
Dominick Baier <dbaier@leastprivilege.com>
2019-02-05 15:07:58 +0100

    added tests for JsonWebKey extensions

---
+++ src/Client/Messages/JsonWebKeyResponse.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
+using IdentityModel.Jwk;
+
+namespace IdentityModel.Client
+{
+    /// <summary>
+    /// Models a response from a JWK endpoint
+    /// </summary>
+    /// <seealso cref="IdentityModel.Client.ProtocolResponse" />
+    public class JsonWebKeyResponse : ProtocolResponse
+    {
+        protected override Task InitializeAsync(object initializationData = null)
+        {
+            if (!HttpResponse.IsSuccessStatusCode)
+            {
+                ErrorMessage = initializationData as string;
+            }
+            else
+            {
+                KeySet = new JsonWebKeySet(Raw);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// The key set
+        /// </summary>
+        public JsonWebKeySet KeySet { get; set; }
+    }
+}
