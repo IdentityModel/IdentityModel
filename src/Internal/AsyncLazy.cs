a3c385ee8ccbd6e507e200dad2fe5d90ce36a6a6
Brice <brice@xcit.org>
2019-05-16 10:31:38 +0200

    Fixed AsyncLazy task factory hanging on Unity/WebGL

--- src/Internal/AsyncLazy.cs
+++ src/Internal/AsyncLazy.cs
@@ -8,10 +8,18 @@ namespace IdentityModel.Internal
 {
     class AsyncLazy<T> : Lazy<Task<T>>
     {
-        public AsyncLazy(Func<Task<T>> taskFactory) :
-            base(() => Task.Factory.StartNew(taskFactory).Unwrap())
+		public AsyncLazy(Func<Task<T>> taskFactory) :
+            base(() => GetTaskAsync(taskFactory).Unwrap())
         { }
 
+		private static async Task<Task<T>> GetTaskAsync(Func<Task<T>> taskFactory)
+		{
+			// Let the task factory run synchronously in its own context.
+			await Task.Yield();
+
+			return taskFactory();
+		}
+		
         //TODO: at some point allow this
         //public AsyncLazy(Func<Task<T>> taskFactory, LazyThreadSafetyMode mode) :
         //    base(() => Task.Factory.StartNew(taskFactory).Unwrap(), mode)
