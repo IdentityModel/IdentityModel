a9d9e21552aa2eb8030ab3f10a6d67714fb7a2e9
Dominick Baier <dbaier@leastprivilege.com>
2019-08-23 12:17:09 +0200

    fix EpochTime

--- src/DateTimeExtensions.cs
+++ src/DateTimeExtensions.cs
@@ -18,15 +18,15 @@ namespace IdentityModel
             var date = dateTime.ToUniversalTime();
             var ticks = date.Ticks - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).Ticks;
             var ts = ticks / TimeSpan.TicksPerSecond;
-            return (int)ts;
+            return ts;
         }
 
         /// <summary>
         /// Converts the given epoch time to a <see cref="DateTime"/> with <see cref="DateTimeKind.Utc"/> kind.
         /// </summary>
-        public static DateTime ToDateTimeFromEpoch(this long intDate)
+        public static DateTime ToDateTimeFromEpoch(this long date)
         {
-            var timeInTicks = intDate * TimeSpan.TicksPerSecond;
+            var timeInTicks = date * TimeSpan.TicksPerSecond;
             return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddTicks(timeInTicks);
         }
     }
