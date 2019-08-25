dffad39399fe3f1811d5ac8f38b7c9e81b248e1f
Dominick Baier <dbaier@leastprivilege.com>
2019-08-25 07:58:45 +0200

    cleanup

--- src/Client/Extensions/JObjectExtensions.cs
+++ src/Client/Extensions/JObjectExtensions.cs
@@ -33,26 +33,27 @@ namespace IdentityModel.Client
                 {
                     foreach (var item in array)
                     {
-                        // String is special because item.ToString(Formatting.None) will result in "/"string/"". The quotes will be added.
-                        // Boolean needs item.ToString otherwise 'true' => 'True'
-                        var value = item.Type == JTokenType.String ?
-                            item.ToString() :
-                            item.ToString(Newtonsoft.Json.Formatting.None);
-                        claims.Add(new Claim(x.Key, value));
+                        claims.Add(new Claim(x.Key, Stringify(item)));
                     }
                 }
                 else
                 {
-                    // String is special because item.ToString(Formatting.None) will result in "/"string/"". The quotes will be added.
-                    // Boolean needs item.ToString otherwise 'true' => 'True'
-                    var value = x.Value.Type == JTokenType.String ? 
-                        x.Value.ToString() : 
-                        x.Value.ToString(Newtonsoft.Json.Formatting.None);
-                    claims.Add(new Claim(x.Key, value));
+                    claims.Add(new Claim(x.Key, Stringify(x.Value)));
                 }
             }
 
             return claims;
+        }
+
+        private static string Stringify(JToken item)
+        {
+            // String is special because item.ToString(Formatting.None) will result in "/"string/"". The quotes will be added.
+            // Boolean needs item.ToString otherwise 'true' => 'True'
+            var value = item.Type == JTokenType.String ?
+                item.ToString() :
+                item.ToString(Newtonsoft.Json.Formatting.None);
+
+            return value;
         }
 
         /// <summary>
