45fd762514c68bb1fbc496948c703290039e2d4c
Dominick Baier <dbaier@leastprivilege.com>
2019-08-25 08:38:04 +0100

    New ClaimComparer (#241)
    * add new claim comparer + tests
    
    * add null check
    
    * updated behavior after review
    
    * add value type to comparison

--- src/ClaimComparer.cs
+++ src/ClaimComparer.cs
@@ -8,72 +8,97 @@ using System.Security.Claims;
 namespace IdentityModel
 {
     /// <summary>
-    /// Claim equality comparer
+    /// Compares two instances of Claim
     /// </summary>
-    public class ClaimComparer : IEqualityComparer<Claim>
+    public class ClaimComparer : EqualityComparer<Claim>
     {
-        private readonly bool _valueAndTypeOnly;
+        /// <summary>
+        /// Claim comparison options
+        /// </summary>
+        public class Options
+        {
+            /// <summary>
+            /// Specifies if the issuer value is being taken into account
+            /// </summary>
+            public bool IgnoreIssuer { get; set; } = false;
+
+            /// <summary>
+            /// Specifies if claim and issuer value comparison should be case-sensitive
+            /// </summary>
+            public bool IgnoreValueCase { get; set; } = false;
+        }
+
+        private readonly Options _options = new Options();
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ClaimComparer"/> class.
+        /// Initializes a new instance of the <see cref="ClaimComparer"/> class with default options.
         /// </summary>
         public ClaimComparer()
-        {
-            _valueAndTypeOnly = false;
-        }
+        { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ClaimComparer"/> class.
+        /// Initializes a new instance of the <see cref="ClaimComparer"/> class with given comparison options.
         /// </summary>
-        /// <param name="compareValueAndTypeOnly">if set to <c>true</c> only type and value are being compared.</param>
-        public ClaimComparer(bool compareValueAndTypeOnly)
+        /// <param name="options">Comparison options.</param>
+        public ClaimComparer(Options options)
         {
-            _valueAndTypeOnly = compareValueAndTypeOnly;
+            _options = options ?? throw new ArgumentNullException(nameof(options));
         }
 
-        /// <summary>
-        /// Determines whether the specified objects are equal.
-        /// </summary>
-        /// <param name="x">The first object to compare.</param>
-        /// <param name="y">The second object to compare.</param>
-        /// <returns>
-        /// true if the specified objects are equal; otherwise, false.
-        /// </returns>
-        public bool Equals(Claim x, Claim y)
+        /// <inheritdoc/>
+        public override bool Equals(Claim x, Claim y)
         {
             if (x == null && y == null) return true;
             if (x == null && y != null) return false;
             if (x != null && y == null) return false;
 
-            if (_valueAndTypeOnly)
+            StringComparison valueComparison = StringComparison.Ordinal;
+            if (_options.IgnoreValueCase == true) valueComparison = StringComparison.OrdinalIgnoreCase;
+
+            var equal = (String.Equals(x.Type, y.Type, StringComparison.OrdinalIgnoreCase) &&
+                         String.Equals(x.Value, y.Value, valueComparison) &&
+                         String.Equals(x.ValueType, y.ValueType, StringComparison.Ordinal));
+
+
+            if (_options.IgnoreIssuer)
             {
-                return (x.Type == y.Type &&
-                        x.Value == y.Value);
+                return equal;
             }
             else
             {
-                return (x.Type == y.Type &&
-                        x.Value == y.Value &&
-                        x.Issuer == y.Issuer &&
-                        x.ValueType == y.ValueType);
+                return (equal && String.Equals(x.Issuer, y.Issuer, valueComparison));
             }
         }
 
-        /// <summary>
-        /// Returns a hash code for this instance.
-        /// </summary>
-        /// <param name="claim">The claim.</param>
-        /// <returns>
-        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
-        /// </returns>
-        public int GetHashCode(Claim claim)
+        /// <inheritdoc/>
+        public override int GetHashCode(Claim claim)
         {
-            if (Object.ReferenceEquals(claim, null)) return 0;
+            if (claim is null) return 0;
 
-            int typeHash = claim.Type?.GetHashCode() ?? 0;
-            int valueHash = claim.Value?.GetHashCode() ?? 0;
+            int typeHash = claim.Type?.ToLowerInvariant().GetHashCode() ?? 0 ^ claim.ValueType?.GetHashCode() ?? 0;
+            int valueHash;
+            int issuerHash;
 
-            return typeHash ^ valueHash;
+            if (_options.IgnoreValueCase)
+            {
+                valueHash = claim.Value?.ToLowerInvariant().GetHashCode() ?? 0;
+                issuerHash = claim.Issuer?.ToLowerInvariant().GetHashCode() ?? 0;
+            }
+            else
+            {
+                valueHash = claim.Value?.GetHashCode() ?? 0;
+                issuerHash = claim.Issuer?.GetHashCode() ?? 0;
+            }
+
+            if (_options.IgnoreIssuer)
+            {
+                return typeHash ^ valueHash;
+                
+            }
+            else
+            {
+                return typeHash ^ valueHash ^ issuerHash;
+            }
         }
     }
 }
