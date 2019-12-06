9315dc350aa01f9d421fbb16f2ed6cea7e2bcfb9
Dominick Baier <dbaier@leastprivilege.com>
2019-12-06 08:49:56 +0100

    updates for netcore3.1

--- samples/HttpClientFactorySample/WebApplication1/Startup.cs
+++ samples/HttpClientFactorySample/WebApplication1/Startup.cs
@@ -22,7 +22,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
 
             services.AddHttpClient();
             services.AddHttpClient("token_client",
@@ -47,25 +47,21 @@ namespace WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(e =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                e.MapDefaultControllerRoute();
             });
         }
     }
