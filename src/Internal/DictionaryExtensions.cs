291609a4353e919afffe5b20c5cb974bae64fd56
Mihir Dilip <mihir_dilip@hotmail.co.uk>
2020-02-21 07:04:53 +0000

    Bug - Not checking for duplicate entry if allowEmpty is true (#284)
    * Bug - Not checking for duplicate entry if allowEmpty is true
    
    Here is the proposed solution. I have not tested it myself but looks like it will work.
    
    * Added check for a missing key
    
    * - Signed UnitTests assembly using existing key.snk to be able to make available internal classes from IdentityModel for UnitTests.
    - Added DictionaryExtensionsTests

--- src/Internal/DictionaryExtensions.cs
+++ src/Internal/DictionaryExtensions.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002f25809ad9fde9869a3ae4558b897c8a23458393921395b9439e03d6a52afadbf6ff65ef1049cd2ee4ca5501976ad45b453dc3780b8fa7eb39bae755163ef92d53403a0da484b79d24de1bb759eedceb1e13416c734d9c48b226fcd26c18e0a525b68cdba9f2395502d7df5a6d45c2478edd52752511e2924ea209f83aaa23a1")]
 namespace IdentityModel.Internal
 {
     internal static class DictionaryExtensions
@@ -11,6 +13,7 @@ namespace IdentityModel.Internal
         public static void AddOptional(this IDictionary<string, string> parameters, string key, string value)
         {
             if (parameters == null) throw new ArgumentNullException(nameof(parameters));
+            if (key.IsMissing()) throw new ArgumentNullException(nameof(key));
 
             if (value.IsPresent())
             {
@@ -28,28 +31,19 @@ namespace IdentityModel.Internal
         public static void AddRequired(this IDictionary<string, string> parameters, string key, string value, bool allowEmpty = false)
         {
             if (parameters == null) throw new ArgumentNullException(nameof(parameters));
-
-            if (value.IsPresent())
+            if (key.IsMissing()) throw new ArgumentNullException(nameof(key));
+            
+            if (parameters.ContainsKey(key))
             {
-                if (parameters.ContainsKey(key))
-                {
-                    throw new InvalidOperationException($"Duplicate parameter: {key}");
-                }
-                else
-                {
-                    parameters.Add(key, value);
-                }
+                throw new InvalidOperationException($"Duplicate parameter: {key}");
+            }
+            else if (value.IsPresent() || allowEmpty)
+            {
+                parameters.Add(key, value);
             }
             else
             {
-                if (allowEmpty)
-                {
-                    parameters.Add(key, "");
-                }
-                else
-                {
-                    throw new ArgumentException("Parameter is required", key);
-                }
+                throw new ArgumentException($"Parameter is required", key);
             }
         }
     }
