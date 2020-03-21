d8d0b21a6a7c75d0a974ce4594f75803afc42ab9
Stefan Nikolei <stefan@nikolei.eu>
2020-03-21 22:36:11 +0100

    remove using newtonsoft.json from testproject

--- test/UnitTests/HttpClientExtensions/HttpClientDiscoveryExtensionsTests.cs
+++ test/UnitTests/HttpClientExtensions/HttpClientDiscoveryExtensionsTests.cs
@@ -3,7 +3,6 @@
 
 using FluentAssertions;
 using IdentityModel.Client;
-using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Linq;
@@ -243,7 +242,7 @@ namespace IdentityModel.UnitTests
                 bar = "bar"
             };
 
-            var handler = new NetworkHandler(JsonConvert.SerializeObject(content), HttpStatusCode.InternalServerError);
+            var handler = new NetworkHandler(JsonSerializer.Serialize(content), HttpStatusCode.InternalServerError);
 
             var client = new HttpClient(handler)
             {
@@ -331,7 +330,7 @@ namespace IdentityModel.UnitTests
 
                     response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
                     {
-                        Content = new StringContent(JsonConvert.SerializeObject(content))
+                        Content = new StringContent(JsonSerializer.Serialize(content))
                     }; 
                 }
 
