ab7f6698ec87ea10b9b21421b48479883a5bb409
Dominick Baier <dbaier@leastprivilege.com>
2020-12-24 11:27:35 +0100

    allow relative URLs on ProtocolRequest

--- src/Client/Messages/ProtocolRequest.cs
+++ src/Client/Messages/ProtocolRequest.cs
@@ -175,7 +175,7 @@ namespace IdentityModel.Client
 
             if (Address.IsPresent())
             {
-                RequestUri = new Uri(Address);
+                RequestUri = new Uri(Address, UriKind.RelativeOrAbsolute);
             }
 
             if (Parameters.Any())
