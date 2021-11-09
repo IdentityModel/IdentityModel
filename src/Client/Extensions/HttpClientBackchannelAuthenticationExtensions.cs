30ea540fa4e266cfdb50ce8b8ffe7f14315923fe
Dominick Baier <dbaier@leastprivilege.com>
2021-11-09 14:27:22 +0100

    first cut

---
+++ src/Client/Extensions/HttpClientBackchannelAuthenticationExtensions.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Internal;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityModel.Client;
+
+/// <summary>
+/// HttpClient extensions CIBA backchannel authentication
+/// </summary>
+public static class HttpClientBackchannelAuthenticationExtensions
+{
+    /// <summary>
+    /// Sends a CIBA backchannel authentication request
+    /// </summary>
+    /// <param name="client">The client.</param>
+    /// <param name="request">The request.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    public static async Task<BackchannelAuthenticationResponse> RequestBackchannelAuthenticationAsync(this HttpMessageInvoker client, BackchannelAuthenticationRequest request, CancellationToken cancellationToken = default)
+    {
+        var clone = request.Clone();
+
+        clone.Parameters.AddRequired(OidcConstants.AuthorizeRequest.Scope, request.Scope);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.ClientNotificationToken, request.ClientNotificationToken);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.AcrValues, request.AcrValues);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.LoginHintToken, request.LoginHintToken);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.LoginHint, request.LoginHint);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.IdTokenHint, request.IdTokenHint);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.BindingMessage, request.BindingMessage);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.UserCode, request.UserCode);
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.RequestedExpiry, request.RequestedExpiry.ToString());
+        clone.Parameters.AddOptional(OidcConstants.BackchannelAuthenticationRequest.Request, request.Request);
+        
+        clone.Method = HttpMethod.Post;
+        clone.Prepare();
+                        
+        HttpResponseMessage response;
+        try
+        {
+            response = await client.SendAsync(clone, cancellationToken).ConfigureAwait();
+        }
+        catch (Exception ex)
+        {
+            return ProtocolResponse.FromException<BackchannelAuthenticationResponse>(ex);
+        }
+
+        return await ProtocolResponse.FromHttpResponseAsync<BackchannelAuthenticationResponse>(response).ConfigureAwait();
+    }
+}
---
+++ src/Client/Messages/BackchannelAuthenticationResponse.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace IdentityModel.Client;
+
+/// <summary>
+/// Models a CIBA backchannel authentication response
+/// </summary>
+/// <seealso cref="IdentityModel.Client.ProtocolResponse" />
+public class BackchannelAuthenticationResponse : ProtocolResponse
+{
+    /// <summary>
+    /// REQUIRED. This is a unique identifier to identify the authentication request made by the Client.
+    /// </summary>
+    public string AuthenticationRequestId =>
+        Json.TryGetString(OidcConstants.BackchannelAuthenticationResponse.AuthenticationRequestId);
+
+    /// <summary>
+    /// REQUIRED. A JSON number with a positive integer value indicating the expiration time of the "auth_req_id" in seconds since the authentication request was received.
+    /// </summary>
+    public int? ExpiresIn => Json.TryGetInt(OidcConstants.BackchannelAuthenticationResponse.ExpiresIn);
+
+    /// <summary>
+    /// OPTIONAL. A JSON number with a positive integer value indicating the minimum amount of time in seconds that the Client MUST wait between polling requests to the token endpoint.
+    /// </summary>
+    public int? Interval => Json.TryGetInt(OidcConstants.BackchannelAuthenticationResponse.Interval);
+}
