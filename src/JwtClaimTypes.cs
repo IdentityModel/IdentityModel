10ac4b29797ae79cdf756c36677ae2cf090e063f
Dominick Baier <dbaier@leastprivilege.com>
2023-01-04 10:59:20 +0100

    first cut

--- src/JwtClaimTypes.cs
+++ src/JwtClaimTypes.cs
@@ -165,11 +165,31 @@ public static class JwtClaimTypes
     /// The confirmation
     /// </summary>
     public const string Confirmation = "cnf";
-
+    
+    /// <summary>
+    /// JSON web key
+    /// </summary>
+    public const string JsonWebKey = "jwk";
+    
+    /// <summary>
+    /// DPoP HTTP method
+    /// </summary>
+    public const string DPoPHttpMethod = "htm";
+    
+    /// <summary>
+    /// DPoP HTTP URL
+    /// </summary>
+    public const string DPoPHttpUrl = "htu";
+    
+    /// <summary>
+    /// DPoP access token hash
+    /// </summary>
+    public const string DPoPAccessTokenHash = "ath";
+    
     /// <summary>
     /// Values for strongly typed JWTs
     /// </summary>
-    public class JwtTypes
+    public static class JwtTypes
     {
         /// <summary>
         /// OAuth 2.0 access token
@@ -180,5 +200,31 @@ public static class JwtClaimTypes
         /// JWT secured authorization request
         /// </summary>
         public const string AuthorizationRequest = "oauth-authz-req+jwt";
+        
+        /// <summary>
+        /// DPoP proof token
+        /// </summary>
+        public const string DPoPProofToken = "dpop+jwt";
+    }
+
+    /// <summary>
+    /// Values for the cnf claim
+    /// </summary>
+    public static class ConfirmationMethods
+    {
+        /// <summary>
+        /// JSON web key
+        /// </summary>
+        public const string JsonWebKey = "jwk";
+        
+        /// <summary>
+        /// JSON web key thumbprint
+        /// </summary>
+        public const string JwkThumbprint = "jkt";
+            
+        /// <summary>
+        /// X.509 certificate thumbprint using SHA256
+        /// </summary>
+        public const string X509ThumbprintSha256 = "x5t#S256";
     }
 }
