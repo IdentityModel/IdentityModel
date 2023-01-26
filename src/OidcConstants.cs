e8a207d7ca4f142b8cb7b57919369be60e1dcea6
Brock Allen <brockallen@gmail.com>
2023-01-26 09:37:46 -0500

    add constant for OIDC prompt=create

--- src/OidcConstants.cs
+++ src/OidcConstants.cs
@@ -338,6 +338,7 @@ public static class OidcConstants
         public const string Login = "login";
         public const string Consent = "consent";
         public const string SelectAccount = "select_account";
+        public const string Create = "create";
     }
 
     public static class CodeChallengeMethods
