8d1976a2cdec71fb477426ca93b05c43dfec45b1
Joe DeCock <josephdecock@gmail.com>
2024-01-12 15:31:37 -0600

    Build and test .NET 8

--- test/UnitTests/Infrastructure/FileName.cs
+++ test/UnitTests/Infrastructure/FileName.cs
@@ -9,7 +9,7 @@ namespace IdentityModel.UnitTests
     {
         public static string Create(string name)
         {
-#if NETCOREAPP2_1 || NETCOREAPP3_1 || NET5_0 || NET6_0 || NET7_0
+#if NETCOREAPP2_1 || NETCOREAPP3_1 || NET5_0 || NET6_0 || NET7_0 || NET8_0
             var fullName = Path.Combine(System.AppContext.BaseDirectory, "documents", name);
 #else
             var fullName = Path.Combine(Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationBasePath, "documents", name);
