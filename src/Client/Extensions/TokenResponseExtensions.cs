df7e5f853de32364db51f8539b531a8c89202ce7
Frost <frostchi6688@gmail.com>
2024-04-19 10:07:20 -0500

    Token EnsureSuccessResponse Extension Method

---
+++ src/Client/Extensions/TokenResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace IdentityModel.Client.Extensions
+{
+    /// <summary>
+    /// Extensions for TokenResponse.
+    /// </summary>
+    public static class TokenResponseExtensions
+    {
+        /// <summary>
+        /// Throws an exception if the IsError property for the Token response is true.
+        /// </summary>
+        /// <param name="tokenResponse">Token Response to validate.</param>
+        /// <exception cref="HttpRequestException"></exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EnsureSuccessResponse(this TokenResponse tokenResponse)
+        {
+            if (tokenResponse.IsError)
+                throw new HttpRequestException($"Token Request was not successful: ({tokenResponse.HttpStatusCode}) {tokenResponse.Error}", tokenResponse.Exception);
+        }
+    }
+}
